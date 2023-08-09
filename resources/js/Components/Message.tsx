f8207bc5d35dfa5343aab24f5338179229c1d860
Felipe Malacarne <felipemalacarne012@gmail.com>
2023-08-09 14:23:56 -0300

    Refactor new component Message

---
+++ resources/js/Components/Message.tsx
@@ -0,0 +1,35 @@
+import { type } from 'os'
+import React from 'react'
+
+interface MessageProps {
+    type: 'Success' | 'Error'
+    txt?: string
+    onClose: () => void
+}
+
+export const Message: React.FC<MessageProps> = (props) => {
+
+    console.log(props.type)
+    console.log(props.txt)
+    return (
+        <div className={
+            props.type == 'Success' ? 'bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative':
+                'bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'} role={'alert'}
+            >
+            <strong className={'font-bold'}>
+                {props.type + '! '}
+            </strong>
+            <span className={'block sm:inline'}>
+                {props.txt} {' ' + (props.type == 'Error') ? 'Please try again.' : 'Your file was uploaded successfully.'}
+            </span>
+            <span className={'absolute top-0 bottom-0 right-0 px-4 py-3'}>
+                <svg className={ 
+                    props.type == 'Success' ? 'fill-current h-6 w-6 text-green-500' :
+                    'fill-current h-6 w-6 text-red-500'} role={'button'} xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 20 20'} onClick={props.onClose}>
+                    <title>Close</title>
+                    <path fillRule={'evenodd'} d={'M14.348 5.652a.5.5 0 010 .707L10.707 10l3.64 3.64a.5.5 0 11-.707.707L10 10.707l-3.64 3.64a.5.5 0 01-.707-.707L9.293 10 5.652 6.36a.5.5 0 01.707-.707L10 9.293l3.64-3.64a.5.5 0 01.708 0z'} clipRule={'evenodd'} />
+                </svg>
+            </span>
+        </div>
+    )
+}
--- resources/js/Pages/ImportPage.tsx
+++ resources/js/Pages/ImportPage.tsx
@@ -4,6 +4,7 @@ import {Head, useForm, usePage, } from "@inertiajs/react";
 import {useState} from "react";
 import PrimaryButton from "@/Components/PrimaryButton";
 import axios from "axios";
+import { Message } from "@/Components/Message";
 
 export default function ImportPage ({ auth,  }: PageProps) {
 
@@ -51,7 +52,6 @@ export default function ImportPage ({ auth,  }: PageProps) {
                       encType={'multipart/form-data'}
                 >
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3 my-3 flex gap-4">
-                        {/*<div className="p-6 text-gray-900">You're logged in!</div>*/}
                         <label id="file-input-label" htmlFor="file-input" className={'inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'}>
                             File Upload
                         </label>
@@ -64,29 +64,15 @@ export default function ImportPage ({ auth,  }: PageProps) {
                             {processing ? 'Loading...' : 'Submit'}
                         </PrimaryButton>
                     </div>
-                    {showErrorMessage && <div className={'bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'} role={'alert'}>
-                        <strong className={'font-bold'}>Error! </strong>
-                        <span className={'block sm:inline'}>{`${errors[0]}`}. Please try again.</span>
-                        <span className={'absolute top-0 bottom-0 right-0 px-4 py-3'}>
-                            <svg className={'fill-current h-6 w-6 text-red-500'} role={'button'} xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 20 20'} onClick={() => setShowErrorMessage(false)}>
-                                <title>Close</title>
-                                <path fillRule={'evenodd'} d={'M14.348 5.652a.5.5 0 010 .707L10.707 10l3.64 3.64a.5.5 0 11-.707.707L10 10.707l-3.64 3.64a.5.5 0 01-.707-.707L9.293 10 5.652 6.36a.5.5 0 01.707-.707L10 9.293l3.64-3.64a.5.5 0 01.708 0z'} clipRule={'evenodd'}/>
-                            </svg>
-                        </span>
-                    </div>}
-                    {showSuccessMessage && <div className={'bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative'} role={'alert'} >
-                        <strong className={'font-bold'}>Success! </strong>
-                        <span className={'block sm:inline'}>Your file was uploaded successfully.</span>
-                        <span className={'absolute top-0 bottom-0 right-0 px-4 py-3'} >
-                            <svg className={'fill-current h-6 w-6 text-green-500'} role={'button'} xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 20 20'} onClick={() => setShowSuccessMessage(false)}>
-                                <title>Close</title>
-                                <path fillRule={'evenodd'} d={'M14.348 5.652a.5.5 0 010 .707L10.707 10l3.64 3.64a.5.5 0 11-.707.707L10 10.707l-3.64 3.64a.5.5 0 01-.707-.707L9.293 10 5.652 6.36a.5.5 0 01.707-.707L10 9.293l3.64-3.64a.5.5 0 01.708 0z'} clipRule={'evenodd'}/>
-                            </svg>
-                        </span>
-                    </div>}
+
+                    {showErrorMessage && <Message type={'Error'} txt={errors[0]} onClose={() => setShowErrorMessage(false)}/>}
+
+                    {showSuccessMessage && <Message type={'Success'} onClose={() => setShowSuccessMessage(false)}/>}
+
                     {data.fileUpload && <div className={'bg-white overflow-hidden shadow-sm sm:rounded-lg p-3 my-3'}>
                         <div className={'p-6 text-gray-900'}>{data.fileUpload.length} files selected</div>
                     </div>}
+
                 </form>
             </div>
         </AuthenticatedLayout>
